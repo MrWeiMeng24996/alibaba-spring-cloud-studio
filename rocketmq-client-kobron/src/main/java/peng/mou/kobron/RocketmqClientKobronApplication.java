82c851b68a4527198314d67a6190bf0bf9d27783
PengMou <mou.peng@outlook.com>
2024-09-16 22:31:02 +0800

    提交一一版

---
+++ rocketmq-client-kobron/src/main/java/peng/mou/kobron/RocketmqClientKobronApplication.java
@@ -0,0 +1,13 @@
+package peng.mou.kobron;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RocketmqClientKobronApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RocketmqClientKobronApplication.class, args);
+    }
+
+}
